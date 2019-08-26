08b21d19e4b1a920cc66ddea454219d9b89e7197
Ottar Søfteland <soeftela@online.no>
2019-08-26 18:42:54 +0200

    correct charset collation

--- init_sql/02_create_db.sql
+++ init_sql/02_create_db.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE IF NOT EXISTS jiradb;
+CREATE DATABASE IF NOT EXISTS jiradb CHARACTER SET utf8_bin COLLATE utf8mb4_unicode_ci;
 GRANT ALL PRIVILEGES ON jiradb.* TO 'jirauser'@'%';
-CREATE DATABASE IF NOT EXISTS confluencedb;
+CREATE DATABASE IF NOT EXISTS confluencedb CHARACTER SET utf8_bin COLLATE utf8mb4_unicode_ci;
 GRANT ALL PRIVILEGES ON confluencedb.* TO 'confuser'@'%';
 
