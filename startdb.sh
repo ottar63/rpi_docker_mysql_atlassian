c23d90852bc4eedb3aa8d40a617c1deca0efd9bd
Ottar Søfteland <soeftela@online.no>
2019-08-28 18:52:10 +0200

    changed image name

--- startdb.sh
+++ startdb.sh
@@ -1,2 +1,2 @@
-docker run --detach --name mysql57 -p 3306:3306 --rm  -v /opt/mysql/data:/var/lib/mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=yes ottar63/rpi_mysql_atlassian
+docker run --detach --name mysql57 -p 3306:3306 --rm  -v /opt/mysql/data:/var/lib/mysql -e MYSQL_ALLOW_EMPTY_PASSWORD=yes ottar63/rpi-mysql-atlassian
 
